6bf3827788a2e9e9627ff662a26c4cbc0746addb
Daniel <dafeamca99@gmail.com>
2021-11-09 17:56:31 -0500

    Creación de pantallas de listas

---
+++ app/src/main/java/com/daca/logwatchlistapp/ListasActivity.java
@@ -0,0 +1,14 @@
+package com.daca.logwatchlistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listas);
+    }
+}
