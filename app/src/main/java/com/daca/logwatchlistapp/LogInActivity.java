6e3cd0ff52e3ec3c5034fa4ffbe4a980f75bf1f3
chokpik <camilod529@gmail.com>
2021-11-09 23:05:43 -0500

    Creacion y autenticacion de usuario lista con firebase

--- app/src/main/java/com/daca/logwatchlistapp/LogInActivity.java
+++ app/src/main/java/com/daca/logwatchlistapp/LogInActivity.java
@@ -1,49 +1,63 @@
 package com.daca.logwatchlistapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity {
 
     private Button btnaccede;
     private EditText etEmail;
     private EditText etPass;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         btnaccede = findViewById(R.id.btn_accede_logIn);
-        etEmail = findViewById(R.id.etEmail_logIn);
+        etEmail = findViewById(R.id.etEmail_signin);
         etPass = findViewById(R.id.etPass_logIn);
+        mAuth = FirebaseAuth.getInstance();
 
-        btnaccede.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String usu = "prueba@correo.com";
-                String pass = "prueba123";
-                String correo = etEmail.getText().toString();
-                String passwd = etPass.getText().toString();
-
-                if(correo.equals(usu) && passwd.equals(pass)){
-                    Toast.makeText(LogInActivity.this, "Bienvenido " + correo, Toast.LENGTH_LONG).show();
-
-                    Intent miIntencion = new Intent(LogInActivity.this, ListasActivity.class);
-                    miIntencion.putExtra("correo",correo);
-                    startActivity(miIntencion);
-                    finish();
-                } else{
-                    Toast.makeText(LogInActivity.this, "Datos errados", Toast.LENGTH_LONG).show();
-                }
-            }
+        btnaccede.setOnClickListener(view -> {
+            String email = etEmail.getText().toString();
+            String pass = etPass.getText().toString();
+            signInFun(email, pass);
         });
 
+    }
+
+    private void signInFun(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d("a", "signInWithEmail:success");
+                            Intent miIntencion = new Intent(LogInActivity.this, ListasActivity.class);
+                            miIntencion.putExtra("correo",email);
+                            startActivity(miIntencion);
+                        }
+                        else {
+                            Log.w("b", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/daca/logwatchlistapp/SignInActivity.java
+++ app/src/main/java/com/daca/logwatchlistapp/SignInActivity.java
@@ -1,14 +1,68 @@
 package com.daca.logwatchlistapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+
+    EditText etCorreo;
+    EditText etPass;
+    Button btncrear;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        etCorreo = findViewById(R.id.etEmail_signin);
+        etPass = findViewById(R.id.etPass_signin);
+        btncrear = findViewById(R.id.btn_crear_signin);
+
+        btncrear.setOnClickListener(view -> {
+            String email = etCorreo.getText().toString();
+            String pass = etPass.getText().toString();
+            createAccount(email,pass);
+        });
+
+
+    }
+
+    private void createAccount(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d("a", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent miIntencion = new Intent(SignInActivity.this, ListasActivity.class);
+                            miIntencion.putExtra("correo",email);
+                            startActivity(miIntencion);
+                        }
+                        else {
+                            Log.w("b", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
